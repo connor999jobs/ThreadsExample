1c4f4fbbcb22b01f7efd0e57dd272b4edbf80641
connor999jobs <connor999jobs@gmail.com>
2022-08-25 16:32:27 +0300

    Add Ping Pong

---
+++ src/main/java/org/example/pingpong/Main.java
@@ -0,0 +1,28 @@
+package org.example.pingpong;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.SneakyThrows;
+
+import java.util.concurrent.Semaphore;
+
+public class Main {
+
+    private final static Semaphore ping = new Semaphore(1);
+    private final static Semaphore pong = new Semaphore(0);
+    private static final int count = 10;
+
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        PingPongSemaphore semaphorePing = new PingPongSemaphore("Ping", ping,pong, count);
+        PingPongSemaphore semaphorePong = new PingPongSemaphore("Pong", pong,ping,count);
+        System.out.println("Start game");
+        semaphorePing.start();
+        semaphorePong.start();
+        semaphorePing.join();
+        semaphorePong.join();
+        System.out.println("Game Over");
+    }
+}
+
---
+++ src/main/java/org/example/pingpong/PingPongSemaphore.java
@@ -0,0 +1,30 @@
+package org.example.pingpong;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.SneakyThrows;
+
+import java.util.concurrent.Semaphore;
+
+
+@Data
+@AllArgsConstructor
+public class PingPongSemaphore extends Thread {
+    private String startPlay;
+    private Semaphore first;
+    private Semaphore second;
+    private int count;
+
+
+    @Override
+    @SneakyThrows
+    public void run() {
+        while (count > 0){
+            first.acquire();
+            System.out.println(startPlay);
+            Thread.sleep(1000);
+            second.release();
+            count--;
+        }
+    }
+}
